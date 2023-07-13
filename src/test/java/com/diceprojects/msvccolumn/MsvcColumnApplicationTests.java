fe2787fa25f42f32e156398a0b7e172ec6dfae13
MartinDiCe <jamartindice@gmail.com>
2023-07-13 13:33:53 -0300

    Push Project

---
+++ src/test/java/com/diceprojects/msvccolumn/MsvcColumnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diceprojects.msvccolumn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcColumnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
